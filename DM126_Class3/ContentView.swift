0a5fe92d465a72369ba429fff8a123d065f31287
Marcelo Carvalho <silva.marcelocarvalho@gmail.com>
2024-11-09 13:25:01 -0300

    Initial Commit

---
+++ DM126_Class3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DM126_Class3
+//
+//  Created by mcarvalhosilva on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
